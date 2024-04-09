e765d2a1ee325970b4e1fd0b4d6fa32a3751de00
quebec2024 <A01287225@tec.mx>
2024-04-09 09:43:28 -0400

    New actions

---
+++ chess/actions.py
@@ -0,0 +1,9 @@
+def check ( king , move, location, board)
+ pass
+def   check mate (screen, move , board)
+pass
+def change of character (color, location, board)
+pass
+def castling (movement , board, location)
+ pass
+ 
