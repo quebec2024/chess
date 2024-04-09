a5d981b620cbe156c59f05bb04493317109a428f
jimenasepulveda07 <165345840+jimenasepulveda07@users.noreply.github.com>
2024-04-09 09:39:27 -0400

    added :

--- chess/board.py
+++ chess/board.py
@@ -1,12 +1,12 @@
-def pawns (color,locaation,board,move)
+def pawns (color,locaation,board,move):
     pass 
-def bishops (color,locaation,board,move)
+def bishops (color,locaation,board,move):
     pass
-def knights (color,locaation,board,move)
+def knights (color,locaation,board,move):
     pass
-def rooks (color,locaation,board,move)
+def rooks (color,locaation,board,move):
     pass
-def queen (color,locaation,board,move)
+def queen (color,locaation,board,move):
     pass
-def king (color,locaation,board,move)
+def king (color,locaation,board,move):
     pass
