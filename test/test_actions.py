0698cbe3bd913ca54cdb4ad160769bbab5be9e06
jimenasepulveda07 <165345840+jimenasepulveda07@users.noreply.github.com>
2024-04-25 09:29:49 -0400

    continued the actions

--- test/test_actions.py
+++ test/test_actions.py
@@ -11,7 +11,42 @@ def test_move():
                     [" "," "," "," "," "," "," "," "],
                     [" "," "," "," "," "," "," "," "],
                     [" "," "," "," "," "," "," "," "]]
+
+def test_verify_pawn_move():
+    if verify_pawn_move([1, 2], [1, 3]):
+        print("True, test passed")
+    else:
+        print("False, test not passed")
     
-# VERUFY PAN MOVE IS TO TELL IF TI WORKS OR NOT
-# true or false
-# starting and ending location 
+def test_verify_pawn_move(starting_position, ending_position):
+
+
+def test_change_of_piece(current_piece, new_piece):
+    if chanage_of_piece ("current_piece", "new_piece") == "new_piece":
+        print("True, test passed")
+    else:
+         print("False, test not passed")
+
+
+def test_verify_check():
+    if verify_check("white") == False:
+        print("True, test passed")
+    else
+        print("False, test not passed")
+    
+    if verify_check("black") == True:
+        print("True, test passed")
+    else
+        print("False, test not passed")
+
+
+def test_verify_check_mate():
+    if verify_check_mate("white") == False:
+        print("True, test passed")
+    else
+        print("False, test not passed")
+    
+    if verify_check_mate("black") == True:
+        print("True, test passed")
+    else
+        print("False, test not passed")
